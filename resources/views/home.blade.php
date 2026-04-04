b6d3a0d42a09aed0f90d154c8f9a8841fb979f6f
MACBOOK <macbook@MacBook-Air-MACBOOK.local>
2026-04-04 17:08:37 +0700

    Add URL Damkar FCM

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -160,18 +160,18 @@
                 if (permStatus.receive === 'granted') {
                     PushNotifications.addListener('registration', (token) => {
                         // 1. Send to Local GMCI Server
-                        // fetch('/public-fcm-token', {
-                        //     method: 'POST',
-                        //     headers: {
-                        //         'Content-Type': 'application/json',
-                        //         'X-CSRF-TOKEN': '{{ csrf_token() }}',
-                        //         'Accept': 'application/json'
-                        //     },
-                        //     body: JSON.stringify({
-                        //         token: token.value,
-                        //         project: 'gmci'
-                        //     })
-                        // }).catch(err => console.error('Local register error:', err));
+                        fetch('/public-fcm-token', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                                'Accept': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                token: token.value,
+                                project: 'gmci'
+                            })
+                        }).catch(err => console.error('Local register error:', err));
 
                         // 2. Send to Central Damkar Server
                         const damkarHeaders = new Headers();
@@ -193,7 +193,6 @@
                             .then((response) => response.json())
                             .then((result) => {
                                 console.log('Damkar register success:', result);
-                                alert('Berhasil mendaftarkan Token ke Damkar');
                             })
                             .catch((error) => {
                                 console.error('Damkar register error:', error);
